ff4ce19df170b53a22dbc9ed6c48663749df4c50
airsoft-gun <k.nezu.rockpalace@gmail.com>
2024-03-14 11:35:51 +0900

    全修正

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-const express = require('express');
+const express = require("express")
 const app = express()
-const {createProxyMiddleware} = require('http-proxy-middleware');
-const rateLimit = require('express-rate-limit');
-require("dotenv").config();
-const url = require('url');
+const { createProxyMiddleware } = require("http-proxy-middleware") 
+const rateLimit = require("express-rate-limit")
+require("dotenv").config()
+const url = require("url")
 
 const limiter = rateLimit({
-    windowMs:15*60*1000,
-    max:10000,
-});
-
-app.get('/',(req,res)=>{
-    res.send("This is my proxy");
+    windowMs: 15 * 60 * 1000,
+    max: 10000, 
 })
 
-app.use("/corona-tracker-country-data",(req,res,next)=>{
-    const city = url.parse(req.url).query
-    createProxyMiddleware({
-        target:`${process.env.BASE_API_URL_CORONA_COUNTRY}/${city}`,
-        changeOrigin:true,
-        pathRewrite:{
-            [`^/corona-tracker-country-data`]:"",
-        },
-    })(req,res,next)
+app.use(limiter)
+
+app.get("/", (req, res) => {  
+    res.send("This is my proxy server")
 })
 
-app.use('/corona-tracker-world-data',limiter,(req,res,next)=>{
+app.use("/corona-tracker-country-data", (req, res, next) => {
+    const city = url.parse(req.url).query 
     createProxyMiddleware({
-        target:process.env.BASE_API_URL_CORONA_WORLD,
-        changeOrigin:true,
-        pathRewrite:{
-            [`^/corona-tracker-world-data`]:"",
+        target: `${process.env.BASE_API_URL_CORONA_COUNTRY}/${city}`,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/corona-tracker-country-data`]: "",
         },
-    })(req,res,next)
+    })(req, res, next)
+})
+
+app.use("/corona-tracker-world-data", (req, res, next) => {
+    createProxyMiddleware({
+        target: process.env.BASE_API_URL_CORONA_WORLD,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/corona-tracker-world-data`]: "",
+        },
+    })(req, res, next)
 })
 
 const port = process.env.PORT || 5000
-app.listen(port,()=>{
-    console.log(`Listening on localhost port ${port}`);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
 
-module.exports = app;
+module.exports = app
